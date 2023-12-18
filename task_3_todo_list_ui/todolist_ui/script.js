90ab96ecdd3a620d0c6aefdb8c0c0c1fa18cc7b0
ME-SKY <meskalitofly@gmail.com>
2023-12-19 05:23:25 +0700

    start of a 3rd task

---
+++ task_3_todo_list_ui/todolist_ui/script.js
@@ -0,0 +1,14 @@
+const APP_STORAGE = {
+    searchString: '',
+    startDate: null, //TODO: change to today later
+    endDate: null,
+    allTodos: [],
+    suggestedTodos: [],
+    filteredTodos: [],
+    openedTodo: null,
+    shortLookTodo: null    
+}
+
+window.onload = function () {
+    console.log('');
+}
